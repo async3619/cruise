59825127940073fe64899ca4e82a4251bb975658
Sophia <beyondthesophia@gmail.com>
2023-03-03 20:18:05 +0900

    chore: use `vite-plugin-electron` package instead of build from scratch

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,47 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import electron from "vite-electron-plugin";
+import { loadViteEnv } from "vite-electron-plugin/plugin";
+import renderer from "vite-plugin-electron-renderer";
+import { rmSync } from "fs";
+
+const devServerUrl = "http://127.0.0.1:7777/";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        react({
-            plugins: [["@swc/plugin-emotion", {}]],
-        }),
-        tsconfigPaths(),
-    ],
-    server: {
-        port: 1420,
-        strictPort: true,
-    },
-    build: {
-        target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-        // don't minify for debug builds
-        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-        // produce sourcemaps for debug builds
-        sourcemap: !!process.env.TAURI_DEBUG,
-    },
+export default defineConfig(({ command }) => {
+    rmSync("dist-electron", { recursive: true, force: true });
+
+    const sourcemap = command === "serve" || !!process.env.VSCODE_DEBUG;
+
+    return {
+        plugins: [
+            react({
+                plugins: [["@swc/plugin-emotion", {}]],
+            }),
+            tsconfigPaths(),
+            electron({
+                include: ["src-main"],
+                transformOptions: {
+                    sourcemap,
+                },
+                plugins: [loadViteEnv()],
+                outDir: "dist-main",
+            }),
+            // Use Node.js API in the Renderer-process
+            renderer({
+                nodeIntegration: true,
+            }),
+        ],
+        server: !!process.env.VSCODE_DEBUG
+            ? (() => {
+                  const url = new URL(devServerUrl);
+                  return {
+                      host: url.hostname,
+                      port: +url.port,
+                  };
+              })()
+            : undefined,
+        clearScreen: false,
+    };
 });
