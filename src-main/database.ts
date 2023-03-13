715491a13a9dfc22ae40b9c9c1afb93e3e215252
Sophia <beyondthesophia@gmail.com>
2023-03-14 07:53:59 +0900

    refactor: typeorm entities are now automatically loaded not hard-coded

--- src-main/database.ts
+++ src-main/database.ts
@@ -1,13 +1,8 @@
 import * as path from "path";
 import * as fs from "fs-extra";
-import { DataSource } from "typeorm";
+import { DataSource, getMetadataArgsStorage } from "typeorm";
 
 import { DATABASE_PATH } from "@main/constants";
-
-import { Album } from "@main/album/models/album.model";
-import { Music } from "@main/music/models/music.model";
-import { Artist } from "@main/artist/models/artist.model";
-import { AlbumArt } from "@main/album-art/models/album-art.model";
 
 export async function initializeDatabase() {
     await fs.ensureDir(path.dirname(DATABASE_PATH));
@@ -15,7 +10,7 @@ export async function initializeDatabase() {
     const dataSource = new DataSource({
         type: "sqlite",
         database: DATABASE_PATH,
-        entities: [Album, Music, Artist, AlbumArt],
+        entities: getMetadataArgsStorage().tables.map(t => t.target),
         synchronize: true,
         dropSchema: true,
     });
