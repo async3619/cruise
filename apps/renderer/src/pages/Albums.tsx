d648cb9c41b121226e98ac944fc72885de0f4f28
Sophia <me@sophia-dev.io>
2023-09-17 18:19:07 +0900

    add shuffle all button on albums page

--- apps/renderer/src/pages/Albums.tsx
+++ apps/renderer/src/pages/Albums.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import { Button } from "ui";
+
+import ShuffleRoundedIcon from "@mui/icons-material/ShuffleRounded";
+
 import { Page } from "@components/Page";
 import { useLibrary } from "@components/Library/context";
 import { AlbumArtistList } from "@components/AlbumArtist/List";
@@ -17,6 +21,18 @@ export function Albums({}: AlbumsProps) {
     const player = usePlayer();
     const { albums, loading } = library.useAlbums();
 
+    const handleShuffleAll = React.useCallback(() => {
+        if (!albums) {
+            return;
+        }
+
+        player.playPlaylist(
+            albums.flatMap(a => a.musics),
+            0,
+            true,
+        );
+    }, [albums, player]);
+
     const handlePlayAlbum = React.useCallback(
         (album: MinimalAlbum) => {
             player.playPlaylist(album.musics, 0, false);
@@ -26,7 +42,23 @@ export function Albums({}: AlbumsProps) {
 
     return (
         <AlbumSelection items={albums}>
-            <Page header={t("pages.albums")} loading={loading} toolbar={<AlbumSelectionToolbar />}>
+            <Page
+                header={t("pages.albums")}
+                loading={loading}
+                toolbar={
+                    <AlbumSelectionToolbar>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            size="small"
+                            startIcon={<ShuffleRoundedIcon />}
+                            onClick={handleShuffleAll}
+                        >
+                            {t("common.shuffle-all")}
+                        </Button>
+                    </AlbumSelectionToolbar>
+                }
+            >
                 <AlbumArtistList items={albums} onPlayAlbum={handlePlayAlbum} />
             </Page>
         </AlbumSelection>
